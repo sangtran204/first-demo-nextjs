89112cfae6bab20dbb18e713fb8db865f7b1563d
tranmausa <currykobejames@gmail.com>
2021-03-09 13:15:14 +0700

    first commit

---
+++ pages/api/user.js
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+// export default function User() {
+//     return (
+//         <div>
+//             <h2>Hello User</h2>
+//         </div>
+//     )
+// }
+
+export default (req, res) => {
+    res.status(200).json({ name: 'hehehehe' })
+  }
